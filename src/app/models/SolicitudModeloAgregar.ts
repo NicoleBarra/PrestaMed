5bb7ee48054d370a2457bc671fd9138f4404b726
NanMed <46913776+NanMed@users.noreply.github.com>
2021-11-25 15:13:40 -0600

    Solicitudes2

---
+++ src/app/models/SolicitudModeloAgregar.ts
@@ -0,0 +1,16 @@
+export interface SolicitudModeloAgregar {
+  _id: string;
+  idProducto: string;
+  name: string;
+	brand: string;
+	description: string;
+	category: string;
+  rentSellSelection: string;
+  status: string;
+  idDuenoProducto: string;
+  idRemitente: string;
+  fechaInicio: string;
+  fechaFin: string;
+  comentario: string;
+  image: string;
+}
--- src/app/module/service/solicitud.service.ts
+++ src/app/module/service/solicitud.service.ts
@@ -4,6 +4,7 @@ import { throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { SolicitudModelo } from 'src/app/models/SolicitudModelo';
 import { SolicitudUsuario } from 'src/app/models/SolicitudUsuario';
+import { SolicitudModeloAgregar } from 'src/app/models/SolicitudModeloAgregar';
 
 @Injectable({
   providedIn: 'root'
@@ -17,12 +18,17 @@ export class SolicitudService {
   endpointMostrarEnviadas = 'http://localhost:8085/api/solicitud/enviadas';
   endpointActualizarEstatus = 'http://localhost:8085/api/solicitud/actualizar';
 
+  endpoint2Agregar = 'http://localhost:8089/api/solicitud/agregar';
+  endpoint2MostrarTodas = 'http://localhost:8089/api/solicitud/showAll';
+  endpoint2MostrarEnviadas = 'http://localhost:8085/api/solicitud/enviadas';
+  endpoint2ActualizarEstatus = 'http://localhost:8085/api/solicitud/actualizar';
+
   insertarSolicitud(solicitudModelo: SolicitudModelo) {
     this.http.post<SolicitudUsuario>(this.endpointAgregar, solicitudModelo).subscribe({
-      next: (data) => {
+      next: (data: any) => {
         console.log('datos', data);
       },
-      error: (error) => {
+      error: (error: any) => {
         console.error(' error!', error);
       },
     });
@@ -42,6 +48,33 @@ export class SolicitudService {
 
   actualizarEstatusSolicitud(solicitudModelo: SolicitudModelo) {
     return this.http.put<SolicitudUsuario>(this.endpointActualizarEstatus, solicitudModelo)
+  }
+
+  insertar2Solicitud(solicitudModeloAgregar: SolicitudModeloAgregar) {
+    this.http.post<SolicitudModeloAgregar>(this.endpoint2Agregar, solicitudModeloAgregar).subscribe({
+      next: (data: any) => {
+        console.log('datos', data);
+      },
+      error: (error: any) => {
+        console.error(' error!', error);
+      },
+    });
+  }
+
+  get2AllSolicitudes(id: string){
+    return this.http
+      .get<SolicitudModelo[]>(this.endpoint2MostrarTodas + '/' + id)
+      .pipe(retry(3), catchError(this.handleError));
+  }
+
+  actualizar2EstatusSolicitud(solicitudModelo: SolicitudModelo) {
+    return this.http.put<SolicitudModelo>(this.endpoint2ActualizarEstatus, solicitudModelo)
+  }
+
+  get2SolicitudesPendientes(id: string){
+    return this.http
+      .get<SolicitudModelo[]>(this.endpoint2MostrarEnviadas + '/' + id)
+      .pipe(retry(3), catchError(this.handleError));
   }
 
   handleError(error: HttpErrorResponse) {
